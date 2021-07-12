30ef5a094f0fbe7fa07375e1fd10bc506f308eab
mvn-hungtran-dn <hung.tran@monstar-lab.com>
2021-07-12 23:34:22 +0700

    finish ex6

--- src/layout/main.jsx
+++ src/layout/main.jsx
@@ -1,21 +1,33 @@
-import React from "react";
-import logo from '../logo.svg';
+import React, { useState, useEffect } from 'react';
 
-export class Main extends React.Component {
-  render() {
-    return (
-      <main className="main">
-        <img
-          src={logo}
-          className="App-logo"
-          alt="logo"
-          width="200"
-          height="200"
-        />
-        <p>
-          Hello react!
-        </p>
-      </main>
-    )
+export function Main() {
+  const [page, setPage] = useState(1)
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetchApi()
+  }, [page])
+
+  const fetchApi = () => {
+    fetch(`https://reqres.in/api/users?page=${page}`)
+    .then(response => response.json())
+    .then(data => setData(data.data))
   }
+
+  return (
+    <div className="list-user">
+      <button type="button" onClick={() => setPage(page - 1)}>Prev</button>
+      {
+        (data || []).map((item, index) => 
+          <div key={index}>
+            <div className="item">
+              <img src={item.avatar}></img>
+              <p>{item.first_name} {item.last_name}</p>
+            </div>
+          </div>
+        )
+      }
+      <button type="button" onClick={() => setPage(page + 1)}>Next</button>
+    </div>
+  );
 }
